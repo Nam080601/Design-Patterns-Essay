857bff43390bfda1fd33748205370c7af09899c0
Lê Tuấn Anh <76466761+githubtuananh@users.noreply.github.com>
2022-05-16 20:31:45 +0700

    Strategy

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        boolean flag = false;
+        System.out.print("Please enter convert type : ");
+        Scanner scanner = new Scanner(System.in);
+        String convertType = scanner.next();
+        System.out.println("Convert type is : " + convertType.replace("2", " to ").toUpperCase());
+        ConvertContext context = new ConvertContext();
+        ConvertFactory[] convertList = { new ConvertXml2JsonFactory(), new ConvertJson2XmlFactory() };
+
+        for (ConvertFactory convert : convertList) {
+            if (!convert.getClass().getSimpleName().equalsIgnoreCase("Convert" + convertType + "Factory")) {
+                continue;
+            }
+
+            context.setConvertStrategy(convert);
+            context.Execute();
+            flag = true;
+        }
+        if (!flag) {
+            System.out.println("Convert type not supported");
+        }
+        scanner.close();
+    }
+}
